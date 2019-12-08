d5159fa91684deeac1c9d1d1f694179d67bb21fa
Shawn Taylor <shawncurtistaylor@gmail.com>
2019-12-08 15:00:24 -0800

    almost finished

---
+++ app.js
@@ -0,0 +1,21 @@
+
+// const mysql = require('mysql');
+
+
+// const con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'user',
+//   password: 'Tupacs6980',
+// });
+
+// con.connect((err) => {
+//   if(err){
+//     console.log('Error connecting to Db');
+//     return;
+//   }
+//   console.log('Connection established');
+// });
+
+// con.end((err) => {
+
+// });
--- sql/README.md
+++ sql/README.md
@@ -1,8 +1,5 @@
 A project to help learn to use mySql.
-
 A way to use selectors to query through a mySql database.
-
 I ran npm test instead of npm run test in the command line to see if I pass the built in tests.
-
-Shawn Taylor San Diego Code School.
+Shawn Taylor, San Diego Code School.
 
